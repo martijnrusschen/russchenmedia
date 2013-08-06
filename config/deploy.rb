225ab33e85cebb44e23b40c453b06854327fcd47
tomdev <tom@north26.nl>
2013-08-06 08:59:51 +0200

    Deploy as 'deploy' user

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,9 @@ server "russchenmedia-production.rusweb.nl", :app, :web, :db, :primary => true
 set :scm, :git
 set :repository, "git@github.com:martijnrusschen/russchenmedia.git"
 set :scm_passphrase, ""
-set :use_sudo, true
 
-set :user, "root"
+set :user, "deploy"
+set :use_sudo, false
 
 set :deploy_to, "/var/www/russchenmedia"
 set :deploy_via, :remote_cache # prevent doing a full repository clean every time
