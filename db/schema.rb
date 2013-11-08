61b5f983720deff14e5e417322efda94cefc1512
Martijn Russchen <martijn@factlink.com>
2013-11-08 17:07:29 +0100

    Added field to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130705104026) do
+ActiveRecord::Schema.define(version: 20131108155936) do
 
   create_table "posts", force: true do |t|
     t.string   "title"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20130705104026) do
     t.string   "youtube_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "client"
+    t.string   "tasks"
+    t.integer  "year"
   end
 
   create_table "rails_admin_histories", force: true do |t|
