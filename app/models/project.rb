1bc762c0342fbee9d86f9f5907c15ccc48594fc3
Martijn Russchen <martijn@factlink.com>
2013-11-08 17:07:53 +0100

    New fields to database

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,9 @@
 #  youtube_id  :string(255)
 #  created_at  :datetime
 #  updated_at  :datetime
+#  client      :string(255)
+#  tasks       :string(255)
+#  year        :integer
 #
 
 class Project < ActiveRecord::Base
