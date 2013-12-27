e205e1f81e0fad6e2c62d0f717ea9da60ee37932
Martijn Russchen <martijn@factlink.com>
2013-12-27 17:14:43 +0100

    Removed migration

--- db/migrate/20131108155936_add_field_to_projects.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldToProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :client, :string
-    add_column :projects, :tasks, :string
-    add_column :projects, :year, :interger
-  end
-end
