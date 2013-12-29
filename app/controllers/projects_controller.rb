2903bbf62b33c4e699c8b3bb8bc67bcad77b5e5e
Martijn Russchen <martijn@factlink.com>
2013-12-29 12:21:54 +0100

    Don't show current project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,12 @@
 class ProjectsController < ApplicationController
+  def all_except_current(project_id)
+    where.not(id: project_id)
+  end
+
   def show
     @project = Project.find(params[:id])
-    @other_projects = Project.last(5)
+    @other_projects = Project.all_except_current(@project)
+                             .order('Year ASC')
+                             .last(5)
   end
 end
