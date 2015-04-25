1c903629b09fd9b847542b0e1ad4ebd4c096478c
Martijn Russchen <martijn@hackerone.com>
2015-04-25 16:42:41 -0700

    Fixed seed to support custom slug

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,12 @@ Project.create  title: "172 Hours",
                 description: "In 2012 nam het Nederlandse Adventure Race team deel aan het wereld Kampioenschap Adventure Racing in Frankrijk. 63 andere internationale top teams deden hetzelfde. Slechts 40 teams slaagden erin de finish te halen.
                               De Raid in France organisatie had een prachtig parcours uitgewerkt dat begon in de Alpen en eindigde aan de kust van de Middellandse zee. Daartussen: 20.000 positieve hoogtemeters, trekkings over gletsjers, mountain biken over ruige bergen en in gevecht tegen het wilde water gedurende de nacht.
                               Beleef het avontuur mee van 1 vrouw en drie mannen met 1 doel: FINISHEN!",
-                youtube_id: "O_JRnZLejPE"
+                youtube_id: "O_JRnZLejPE",
+                slug: "172-hours"
 
 Project.create  title: "The Making Of - Double Exposure: Canon vs. Nikon",
                 description: "Double Exposure: Canon vs. Nikon is een resultaat van een project dat begon met het idee om een gave video te maken met de kracht van de Tweakers.net community. Na maanden van hard werken is de video op dinsdag 13 december in premiere gegaan. Binnen een week leverde dit meer dan een half miljoen kijkers op. Een resultaat waar we erg trots op zijn. In deze video geven we jullie een kijkje achter de schermen.",
-                youtube_id: "eoUhtW_m2u4"
+                youtube_id: "eoUhtW_m2u4",
+                slug: "double-exposure"
 
 User.create!({:email => "martijn@factlink.com", :password => "12345hoi", :password_confirmation => "12345hoi" })
