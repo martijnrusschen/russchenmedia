9f38e049b20cd68827f8e564fad1c89ed5b1b90a
Martijn Russchen <martijn@hackerone.com>
2015-04-25 17:16:14 -0700

    Added project spec

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe ProjectsController, type: :controller do
+  describe '#show' do
+    render_views
+    let(:project) { create :project }
+
+    subject { get :show, id: project.slug }
+
+    it { is_expected.to render_template 'projects/show' }
+    it { expect(subject.status).to eq 200 }
+    it { expect(subject.body).to include project.title }
+  end
+end
