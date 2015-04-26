901b9982678de870f348f7f305004a0d85df294d
Martijn Russchen <martijn@hackerone.com>
2015-04-25 18:05:21 -0700

    Exclude User model

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -26,7 +26,7 @@ RailsAdmin.config do |config|
   # config.default_items_per_page = 20
 
   # Exclude specific models (keep the others):
-  # config.excluded_models = ['Project']
+  config.excluded_models = ['User']
 
   # Include specific models (exclude the others):
   # config.included_models = ['Project']
