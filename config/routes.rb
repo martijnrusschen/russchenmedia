790a2536151d6f67c5f37ee453f093f1b94df0a8
Martijn Russchen <martijn@factlink.com>
2017-04-17 14:58:05 +0200

    Bring back old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #Old urls to root
+  get "/rss" => redirect("/")
+  get "/nieuws" => redirect("/")
+  get "/nieuws_bekijken" => redirect("/")
+  get "/project.php" => redirect("/")
+  get "/projecten" => redirect("/")
+  get "/wiezijnwij" => redirect("/")
+  get "/index" => redirect("/")
+  get "/contact" => redirect("/")
+  get "/sitemap" => redirect("/")
+
+  devise_for :users
+  mount RailsAdmin::Engine => '/zamboya', :as => 'rails_admin'
+  root 'home#index'
+
+  resources :projects, only: [:index, :show], path: '/'
 end
