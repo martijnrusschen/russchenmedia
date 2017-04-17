0860d1bb7b19a9ea46bc8da5e603b0b708179576
Martijn Russchen <martijn@factlink.com>
2017-04-17 15:35:03 +0200

    Update all sort of gems

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Rails.application.configure do
     url: ':s3_domain_url',
     path: ':class/:id.:style.:extension',
     :s3_protocol => 'https',
+    :s3_region => ENV['S3_REGION'],
     :s3_credentials => {
       :bucket => ENV['BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
