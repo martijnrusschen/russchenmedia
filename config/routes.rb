5c5a473a404594c357764a3d3ba8b9f02017e9bb
Martijn Russchen <martijn@factlink.com>
2017-04-17 14:48:25 +0200

    New config

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
-Russchenmedia::Application.routes.draw do
-  #Old urls to root
-  get "/rss" => redirect("/")
-  get "/nieuws" => redirect("/")
-  get "/nieuws_bekijken" => redirect("/")
-  get "/project.php" => redirect("/")
-  get "/projecten" => redirect("/")
-  get "/wiezijnwij" => redirect("/")
-  get "/index" => redirect("/")
-  get "/contact" => redirect("/")
-  get "/sitemap" => redirect("/")
-
-  devise_for :users
-  mount RailsAdmin::Engine => '/zamboya', :as => 'rails_admin'
-  root 'home#index'
-
-  resources :projects, only: [:index, :show], path: '/'
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
