4b7229da42150efd1b8b993a3c192d582574f9e6
Martijn Russchen <martijn@factlink.com>
2017-04-23 00:46:48 +0200

    Add JSON endpoint to home controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,12 +3,35 @@ require 'spec_helper'
 describe HomeController, type: :controller do
   render_views
 
-  describe 'GET index' do
-    it 'renders the index' do
-      3.times { create :project }
-      get :index
+  subject { get :index, format: format }
 
-      expect(response.status).to eq 200
+  describe 'GET index' do
+    context 'format HTML' do
+      let(:format) { :html }
+      let!(:projects) { 3.times { create :project } }
+
+      it 'renders the index' do
+        expect(subject.status).to eq 200
+      end
+    end
+
+    context 'format JSON' do
+      let(:format) { :json }
+      let!(:project) { create :project, title: 'Project 1', slug: 'project-1' }
+
+      before do
+        Timecop.freeze(Time.local(1990))
+      end
+
+      after do
+        Timecop.return
+      end
+
+      it 'works' do
+        verify fomat: :json do
+          subject.body
+        end
+      end
     end
   end
 end
