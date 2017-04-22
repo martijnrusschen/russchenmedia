08a6fdefc26b476b2887a39a166a9ddd03d7371a
Martijn Russchen <martijn@factlink.com>
2017-04-23 00:36:35 +0200

    Support JSON output for projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController < ApplicationController
   def show
     @project = Project.find_by_slug params[:id]
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @project }
+    end
   end
 end
