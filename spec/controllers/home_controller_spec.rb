d493ab29b1f0db38495e0b6c94e15c8961772824
Martijn Russchen <martijn@factlink.com>
2017-04-23 11:36:21 +0200

    Fix home controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -3,34 +3,32 @@ require 'spec_helper'
 describe HomeController, type: :controller do
   render_views
 
+  before do
+    Timecop.freeze(Time.local(1990))
+  end
+
+  after do
+    Timecop.return
+  end
+
   subject { get :index, format: format }
 
   describe 'GET index' do
     context 'format HTML' do
       let(:format) { :html }
-      let!(:projects) { 3.times { create :project } }
 
       it 'renders the index' do
+        3.times { create :project }
         expect(subject.status).to eq 200
       end
     end
 
     context 'format JSON' do
       let(:format) { :json }
-      let!(:project) { create :project, title: 'Project 1', slug: 'project-1' }
-
-      before do
-        Timecop.freeze(Time.local(1990))
-      end
-
-      after do
-        Timecop.return
-      end
 
       it 'works' do
-        verify fomat: :json do
-          subject.body
-        end
+        create :project, title: 'Project 1', slug: 'project-1'
+        verify fomat: :json { subject.body }
       end
     end
   end
